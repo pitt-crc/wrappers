a11d8bd5511e1d38b2d1e77f8a5c0b3409de5a42
Daniel Perrefort <djperrefort@gmail.com>
2022-02-28 10:12:57 -0500

    Patches formatting of SUs string

--- crc-sus.py
+++ crc-sus.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-''' crc-sus.py -- Get SUs from crc-bank.db
+"""crc-sus.py -- Get SUs from crc-bank.db
 Usage:
-    crc-sus.py <account> 
+    crc-sus.py <account>
     crc-sus.py -h | --help
     crc-sus.py -v | --version
 
@@ -11,32 +11,27 @@ Positional Arguments:
 Options:
     -h --help                       Print this screen and exit
     -v --version                    Print the version of crc-sus.py
-'''
-
-
-# Test:
-# 1. Make sure item exists
-def check_item_in_table(table, account):
-    if table.find_one(account=account) is None:
-        exit("ERROR: This account has no SUs?")
-
+"""
 
 import dataset
 from docopt import docopt
 
-# clusters
-clusters = ["smp", "gpu", "mpi", "htc"]
-
-# The magical mystical docopt line
 arguments = docopt(__doc__, version='crc-sus.py version 0.0.1')
 
-# Connect to the database and get the limits table
-# Absolute path ////
+# Connect to the database and get the table with proposal service units
 db = dataset.connect('sqlite:////ihome/crc/bank/crc_bank.db')
 table = db['proposal']
 
-# Print out SUs
-string = "Account {0} on {1}"
-od = table.find_one(account=arguments['<account>'])
-sus = ["cluster {cluster} has {od[cluster]} SUs" for cluster in clusters]
-print(string.format(arguments['<account>'], ", ".join(sus)))
+# Ensure a proposal exists for the given account
+db_record = table.find_one(account=arguments['<account>'])
+if db_record is None:
+    exit('ERROR: No proposal for the given account was found')
+
+# Convert cluster SUs to strings
+clusters = ('smp', 'gpu', 'mpi', 'htc')
+sus_string = ['cluster {} has {} SUs'.format(cluster, db_record[cluster]) for cluster in clusters]
+
+string_prefix = 'Account {}'.format(arguments['<account>'])
+string_postfix = ', '.join(sus_string)
+out_string = ' '.join((string_prefix, string_postfix))
+print(out_string)
