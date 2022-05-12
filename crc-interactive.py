195e9512678ea4e8c38ecaece384d8a459f23e6a
Daniel <djperrefort@gmail.com>
2022-05-12 15:50:20 -0400

    Organizational edits to crc-interavtive.py

--- crc-interactive.py
+++ crc-interactive.py
@@ -28,14 +28,23 @@ Options:
     -o --openmp                     Run using OpenMP style submission
 """
 
+from shlex import split
+from subprocess import Popen, PIPE
+
+from docopt import docopt
+
+arguments = docopt(__doc__, version='crc-interactive.py version 0.0.1')
+
 
 def check_integer_argument(arguments, key):
     if arguments[key]:
         try:
             return int(arguments[key])
+
         except ValueError:
             print("WARNING: {0} should have been an integer, setting {0} to 1 hr".format(key))
             return 1
+
     else:
         return 1
 
@@ -43,43 +52,37 @@ def check_integer_argument(arguments, key):
 def add_to_srun_args(srun_args, srun_dict, arguments, item):
     if arguments[item]:
         srun_args += ' {} '.format(srun_dict[item]).format(arguments[item])
+
     return srun_args
 
 
 def run_command(command, echo=False):
     if echo:
         print(command)
-        return
+
     else:
-        sp = Popen(split(command), stdout=PIPE, stderr=PIPE)
-        return sp.communicate()
+        return Popen(split(command), stdout=PIPE, stderr=PIPE).communicate()
 
 
 def run_command_fg(command, echo=False):
     if echo:
         print(command)
-        return
+
     else:
-        sp = Popen(split(command))
-        return sp.communicate()
+        return Popen(split(command)).communicate()
 
 
 try:
-    # Some imports functions and libraries
-    from docopt import docopt
-    from os import system, uname
-    from subprocess import Popen, PIPE
-    from shlex import split
-
-    arguments = docopt(__doc__, version='crc-interactive.py version 0.0.1')
-
     # Set defaults if necessary
     if not arguments['--time']:
         arguments['--time'] = 1
+
     if not arguments['--num-nodes']:
         arguments['--num-nodes'] = 1
+
     if not arguments['--num-cores']:
         arguments['--num-cores'] = 1
+
     if not arguments['--num-gpus']:
         arguments['--num-gpus'] = 0
 
@@ -106,6 +109,7 @@ try:
 
     if arguments["--openmp"]:
         srun_dict["--num-cores"] = "--cpus-per-task={}"
+
     else:
         srun_dict["--num-cores"] = "--ntasks-per-node={}"
 
@@ -119,8 +123,10 @@ try:
     srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--account')
     if arguments['--gpu'] or arguments['--invest']:
         srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--num-gpus')
+
     if arguments['--license']:
         srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--license')
+
     if arguments['--feature']:
         srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--feature')
 
@@ -132,28 +138,36 @@ try:
         x11_out, x11_err = run_command("xset q")
         if len(x11_err) == 0:
             srun_args += ' --x11 '
+
     except OSError:
         pass
 
     # set echo
     if arguments['--print-command']:
         echo = True
+
     else:
         echo = False
 
     # Run the commands
     if arguments['--smp']:
         run_command_fg("srun -M smp {} --pty bash".format(srun_args), echo)
+
     elif arguments['--gpu']:
         run_command_fg('srun -M gpu {} --pty bash'.format(srun_args), echo)
+
     elif arguments['--mpi']:
         if (not arguments['--partition'] == 'compbio') and arguments['--num-nodes'] < 2:
             exit('Error: You must use more than 1 node on the MPI cluster')
+
         run_command_fg('srun -M mpi {} --pty bash'.format(srun_args), echo)
+
     elif arguments['--invest']:
         if not arguments['--partition']:
             exit("Error: You must specify a partition when using the Investor cluster")
+
         run_command_fg('srun -M invest {} --pty bash'.format(srun_args), echo)
+
     elif arguments['--htc']:
         run_command_fg("srun -M htc {} --pty bash".format(srun_args), echo)
 
