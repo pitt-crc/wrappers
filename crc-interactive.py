f584d19052f8d7fec14f6af857448af4caa1544a
Daniel <djperrefort@gmail.com>
2022-05-12 20:17:33 -0400

    Breaks code into SRP functions

--- crc-interactive.py
+++ crc-interactive.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env /ihome/crc/wrappers/py_wrap.sh
+#!/usr/bin/python
 """crc-interactive.py -- An interactive Slurm helper
 Usage:
     crc-interactive.py (-s | -g | -m | -i | -d) [-hvzo] [-t <time>] [-n <num-nodes>]
@@ -33,29 +33,63 @@ from subprocess import Popen, PIPE
 
 from docopt import docopt
 
-# Parse command line options and set defaults
-arguments = docopt(__doc__, version='crc-interactive.py version 0.0.1')
-arguments.setdefault('--time', 1)
-arguments.setdefault('--num-nodes', 1)
-arguments.setdefault('--num-cores', 1)
-if arguments["--gpu"]:
-    arguments.setdefault('--num-gpus', 1)
-
-else:
-    arguments.setdefault('--num-gpus', 0)
+__VERSION__ = '0.0.2'
+MINIMUM_MPI_NODES = 2
 
 
-def check_integer_argument(arguments, key):
-    if arguments[key]:
+def parse_args():
+    """Parse arguments from the command line and return them as a dictionary
+
+    Includes default values for any unspecified values
+
+    Returns:
+        A dictionary of parsed arguments from the command line
+    """
+
+    arguments = docopt(__doc__, version='{} version {}'.format(__file__, __VERSION__))
+
+    # Set default values
+    arguments.setdefault('--time', 1)
+    arguments.setdefault('--num-nodes', 1)
+    arguments.setdefault('--num-cores', 1)
+    arguments.setdefault('--time', 1)
+    arguments.setdefault('--num-nodes', 1)
+    arguments.setdefault('--num-cores', 1)
+    arguments.setdefault('--mem', 1)
+    if arguments["--gpu"]:
+        arguments.setdefault('--num-gpus', 1)
+
+    else:
+        arguments.setdefault('--num-gpus', 0)
+
+    # This is here to maintain the same behavior as older application versions
+    for key in ['--time', '--num-nodes', '--num-cores', '--num-gpus', '--mem']:
         try:
-            return int(arguments[key])
+            int(arguments[key])
 
         except ValueError:
             print("WARNING: {0} should have been an integer, setting {0} to 1 hr".format(key))
-            return 1
+            arguments[key] = 1
 
-    else:
-        return 1
+    return arguments
+
+
+def validate_arguments(arguments):
+    """Exit the application if command-line arguments are invalid
+
+    Args:
+        arguments: A dictionary of parsed command line arguments
+    """
+
+    # Check wall time is between limits
+    if not (1 <= arguments['--time'] <= 12):
+        exit("ERROR: {} is not in 1 <= time <= 12... exiting".format(arguments['--time']))
+
+    if arguments['--mpi'] and (not arguments['--partition'] == 'compbio') and arguments['--num-nodes'] < MINIMUM_MPI_NODES:
+        exit('Error: You must use more than 1 node on the MPI cluster')
+
+    if arguments['--invest'] and not arguments['--partition']:
+        exit("Error: You must specify a partition when using the Investor cluster")
 
 
 def add_to_srun_args(srun_args, srun_dict, arguments, item):
@@ -65,27 +99,11 @@ def add_to_srun_args(srun_args, srun_dict, arguments, item):
     return srun_args
 
 
-def run_command(command, stdout=None, stderr=None, echo=False):
-    print(command)
-    if echo:
-        print(command)
-
-    else:
-        return Popen(split(command), stdout=stdout, stderr=stderr).communicate()
+def run_command(command, stdout=None, stderr=None):
+    return Popen(split(command), stdout=stdout, stderr=stderr).communicate()
 
 
-try:
-    # Check the integer arguments
-    arguments['--time'] = check_integer_argument(arguments, '--time')
-    arguments['--num-nodes'] = check_integer_argument(arguments, '--num-nodes')
-    arguments['--num-cores'] = check_integer_argument(arguments, '--num-cores')
-    arguments['--num-gpus'] = check_integer_argument(arguments, '--num-gpus')
-    arguments['--mem'] = check_integer_argument(arguments, '--mem')
-
-    # Check walltime is between limits
-    if not (1 <= arguments['--time'] <= 12):
-        exit("ERROR: {} is not in 1 <= time <= 12... exiting".format(arguments['--time']))
-
+def create_srun_command(arguments):
     # Build up the srun arguments
     srun_dict = {'--partition': '--partition={}', '--num-nodes': '--nodes={}',
                  '--time': '--time={}:00:00', '--reservation': '--reservation={}', '--num-gpus': '--gres=gpu:{}',
@@ -106,6 +124,7 @@ try:
     srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--reservation')
     srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--mem')
     srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--account')
+
     if arguments['--gpu'] or arguments['--invest']:
         srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--num-gpus')
 
@@ -127,29 +146,22 @@ try:
     except OSError:
         pass
 
-    echo = bool(arguments['--print-command'])
+    cluster_names = ('smp', 'gpu', 'mpi', 'invest', 'htc')
+    cluster_to_run = next(cluster for cluster in cluster_names if arguments[cluster])
+    return "srun -M {} {} --pty bash".format(cluster_to_run, srun_args)
 
-    # Run the commands
-    if arguments['--smp']:
-        run_command("srun -M smp {} --pty bash".format(srun_args), echo)
 
-    elif arguments['--gpu']:
-        run_command('srun -M gpu {} --pty bash'.format(srun_args), echo)
+if __name__ == '__main__':
+    _arguments = parse_args()
+    validate_arguments(_arguments)
+    srun_command = create_srun_command(_arguments)
 
-    elif arguments['--mpi']:
-        if (not arguments['--partition'] == 'compbio') and arguments['--num-nodes'] < 2:
-            exit('Error: You must use more than 1 node on the MPI cluster')
+    if _arguments['--print-command']:
+        print(srun_command)
+        exit()
 
-        run_command('srun -M mpi {} --pty bash'.format(srun_args), echo)
+    try:
+        run_command(srun_command)
 
-    elif arguments['--invest']:
-        if not arguments['--partition']:
-            exit("Error: You must specify a partition when using the Investor cluster")
-
-        run_command('srun -M invest {} --pty bash'.format(srun_args), echo)
-
-    elif arguments['--htc']:
-        run_command("srun -M htc {} --pty bash".format(srun_args), echo)
-
-except KeyboardInterrupt:
-    exit('Interrupt detected! exiting...')
+    except KeyboardInterrupt:
+        exit('Interrupt detected! exiting...')
