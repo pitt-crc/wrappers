2925f864d9ce9762288c8800853d7e1800d7cbfe
Daniel Perrefort <djperrefort@gmail.com>
2022-05-24 12:17:34 -0400

    Refactor crc-interactive.py (#11)
    Refactors crc-interactive.py
    
    - Moves the majority of code out of the global try and into functions
    - Adds documentation to each of the functions
    - Adds script settings as global variables at the top of the file
    - Formats the code to be PEP8 compliant

--- crc-interactive.py
+++ crc-interactive.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env /ihome/crc/wrappers/py_wrap.sh
+#!/usr/bin/python3.6 -E
 """crc-interactive.py -- An interactive Slurm helper
 Usage:
     crc-interactive.py (-s | -g | -m | -i | -d) [-hvzo] [-t <time>] [-n <num-nodes>]
@@ -28,134 +28,151 @@ Options:
     -o --openmp                     Run using OpenMP style submission
 """
 
+from copy import deepcopy
+from shlex import split
+from subprocess import Popen, PIPE
 
-def check_integer_argument(arguments, key):
-    if arguments[key]:
+from docopt import docopt
+
+__VERSION__ = '0.1.0'
+
+# IMPORTANT: Remember to update the module docstring when changing global values
+
+MINIMUM_MPI_NODES = 2  # Minimum limit on requested MPI nodes
+MINIMUM_TIME = 1  # Minimum limit on requested time in hours
+MAXIMUM_TIME = 12  # Maximum limit on requested time in hours
+
+_arguments = docopt(__doc__, version='{} version {}'.format(__file__, __VERSION__))
+DEFAULT_ARGUMENTS = {
+    '--time': 1,
+    '--num-nodes': 1,
+    '--num-cores': 1,
+    '--mem': 1,
+    '--num-gpus': 1 if _arguments["--gpu"] else 0
+}
+
+
+def set_default_args(arguments, **default_values):
+    """Set default values for parsed arguments
+
+    Args:
+        arguments: A dictionary of parsed command line arguments
+        **default_values: Argument names and their default values
+
+    Returns:
+        A copy of the ``arguments`` argument updated with default values
+    """
+
+    # Set default value for unspecified arguments
+    arguments = deepcopy(arguments)
+    for arg_name, default in default_values.items():
+        if arguments[arg_name] is None:
+            arguments[arg_name] = default
+
         try:
-            return int(arguments[key])
+            int(arguments[arg_name])
+
         except ValueError:
-            print("WARNING: {0} should have been an integer, setting {0} to 1 hr".format(key))
-            return 1
-    else:
-        return 1
+            print("WARNING: {0} should have been an integer, setting {0} to 1 hr".format(arg_name))
+            arguments[arg_name] = default
+
+    return arguments
 
 
-def add_to_srun_args(srun_args, srun_dict, arguments, item):
-    if arguments[item]:
-        srun_args += ' {} '.format(srun_dict[item]).format(arguments[item])
-    return srun_args
+def validate_arguments(arguments):
+    """Exit the application if command-line arguments are invalid
+
+    Args:
+        arguments: A dictionary of parsed command line parsed_args
+    """
+
+    # Check wall time is between limits
+    if not (MINIMUM_TIME <= int(arguments['--time']) <= MAXIMUM_TIME):
+        exit("ERROR: {} is not in {} <= time <= {}... exiting".format(arguments['--time'], MINIMUM_TIME, MAXIMUM_TIME))
+
+    if arguments['--mpi'] and (not arguments['--partition'] == 'compbio') and arguments['--num-nodes'] < MINIMUM_MPI_NODES:
+        exit('ERROR: You must use at least {} node when using the MPI cluster'.format(MINIMUM_MPI_NODES))
+
+    if arguments['--invest'] and not arguments['--partition']:
+        exit("ERROR: You must specify a partition when using the Investor cluster")
 
 
-def run_command(command, echo=False):
-    if echo:
-        print(command)
-        return
-    else:
-        sp = Popen(split(command), stdout=PIPE, stderr=PIPE)
-        return sp.communicate()
+def run_command(command, stdout=None, stderr=None):
+    """Execute a child program in a new process
+
+    Args:
+        command: The command to execute in the subprocess
+        stdout: Optionally route STDOUT from the child process
+        stderr: Optionally route STDERR from the child process
+
+    Returns:
+        The submitted child application as a ``Popen`` instance
+    """
+
+    return Popen(split(command), stdout=stdout, stderr=stderr).communicate()
 
 
-def run_command_fg(command, echo=False):
-    if echo:
-        print(command)
-        return
-    else:
-        sp = Popen(split(command))
-        return sp.communicate()
+def create_srun_command(arguments):
+    """Create an ``srun`` command based on parsed commandline arguments
 
+    Args:
+        arguments: A dictionary of parsed command line parsed_args
 
-try:
-    # Some imports functions and libraries
-    from docopt import docopt
-    from os import system, uname
-    from subprocess import Popen, PIPE
-    from shlex import split
+    Return:
+        The equivalent ``srun`` command as a string
+    """
 
-    arguments = docopt(__doc__, version='crc-interactive.py version 0.0.1')
+    # Map arguments from the parent application to equivalent srun arguments
+    srun_dict = {
+        '--partition': '--partition={}',
+        '--num-nodes': '--nodes={}',
+        '--time': '--time={}:00:00',
+        '--reservation': '--reservation={}',
+        '--mem': '--mem={}g',
+        '--account': '--account={}',
+        '--license': '--licenses={}',
+        '--feature': '--constraint={}',
+        '--num-cores': "--cpus-per-task={}" if arguments["--openmp"] else "--ntasks-per-node={}"
+    }
 
-    # Set defaults if necessary
-    if not arguments['--time']:
-        arguments['--time'] = 1
-    if not arguments['--num-nodes']:
-        arguments['--num-nodes'] = 1
-    if not arguments['--num-cores']:
-        arguments['--num-cores'] = 1
-    if not arguments['--num-gpus']:
-        arguments['--num-gpus'] = 0
+    # Build a string of srun arguments
+    srun_args = ''
+    for app_arg_name, srun_arg_name in srun_dict.items():
+        arg_value = arguments[app_arg_name]
+        if arg_value:
+            srun_args += ' ' + srun_arg_name.format(arg_value)
 
-    # Make sure GPU has default of 1
-    if arguments["--gpu"] and arguments["--num-gpus"] == 0:
-        arguments["--num-gpus"] = 1
+    # The --gres argument in srun needs some special handling so is missing from the above dict
+    if (arguments['--gpu'] or arguments['--invest']) and arguments['--num-gpus']:
+        srun_args += ' ' + '--gres=gpu:{}'.format(arguments['--num-gpus'])
 
-    # Check the integer arguments
-    arguments['--time'] = check_integer_argument(arguments, '--time')
-    arguments['--num-nodes'] = check_integer_argument(arguments, '--num-nodes')
-    arguments['--num-cores'] = check_integer_argument(arguments, '--num-cores')
-    arguments['--num-gpus'] = check_integer_argument(arguments, '--num-gpus')
-    arguments['--mem'] = check_integer_argument(arguments, '--mem')
+    srun_args += ' --export=ALL'
 
-    # Check walltime is between limits
-    if not (arguments['--time'] >= 1 and arguments['--time'] <= 12):
-        exit("ERROR: {} is not in 1 <= time <= 12... exiting".format(arguments['--time']))
-
-    # Build up the srun arguments
-    srun_dict = {'--partition': '--partition={}', '--num-nodes': '--nodes={}',
-                 '--time': '--time={}:00:00', '--reservation': '--reservation={}', '--num-gpus': '--gres=gpu:{}',
-                 '--mem': '--mem={}g', '--account': '--account={}', '--license': '--licenses={}',
-                 '--feature': '--constraint={}'}
-
-    if arguments["--openmp"]:
-        srun_dict["--num-cores"] = "--cpus-per-task={}"
-    else:
-        srun_dict["--num-cores"] = "--ntasks-per-node={}"
-
-    srun_args = ""
-    srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--partition')
-    srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--num-nodes')
-    srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--num-cores')
-    srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--time')
-    srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--reservation')
-    srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--mem')
-    srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--account')
-    if arguments['--gpu'] or arguments['--invest']:
-        srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--num-gpus')
-    if arguments['--license']:
-        srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--license')
-    if arguments['--feature']:
-        srun_args = add_to_srun_args(srun_args, srun_dict, arguments, '--feature')
-
-    # Export MODULEPATH
-    srun_args += ' --export=ALL '
-
-    # Add --x11 flag?
+    # Add the --x11 flag only if X11 is working
     try:
-        x11_out, x11_err = run_command("xset q")
-        if len(x11_err) == 0:
+        x11_out, x11_err = run_command("xset q", stdout=PIPE, stderr=PIPE)
+        if not x11_err:
             srun_args += ' --x11 '
+
     except OSError:
         pass
 
-    # set echo
-    if arguments['--print-command']:
-        echo = True
-    else:
-        echo = False
+    cluster_names = ('smp', 'gpu', 'mpi', 'invest', 'htc')
+    cluster_to_run = next(cluster for cluster in cluster_names if arguments.get('--' + cluster))
+    return "srun -M {} {} --pty bash".format(cluster_to_run, srun_args)
 
-    # Run the commands
-    if arguments['--smp']:
-        run_command_fg("srun -M smp {} --pty bash".format(srun_args), echo)
-    elif arguments['--gpu']:
-        run_command_fg('srun -M gpu {} --pty bash'.format(srun_args), echo)
-    elif arguments['--mpi']:
-        if (not arguments['--partition'] == 'compbio') and arguments['--num-nodes'] < 2:
-            exit('Error: You must use more than 1 node on the MPI cluster')
-        run_command_fg('srun -M mpi {} --pty bash'.format(srun_args), echo)
-    elif arguments['--invest']:
-        if not arguments['--partition']:
-            exit("Error: You must specify a partition when using the Investor cluster")
-        run_command_fg('srun -M invest {} --pty bash'.format(srun_args), echo)
-    elif arguments['--htc']:
-        run_command_fg("srun -M htc {} --pty bash".format(srun_args), echo)
 
-except KeyboardInterrupt:
-    exit('Interrupt detected! exiting...')
+if __name__ == '__main__':
+    _args_with_defaults = set_default_args(_arguments, **DEFAULT_ARGUMENTS)
+    validate_arguments(_args_with_defaults)
+    srun_command = create_srun_command(_args_with_defaults)
+
+    if _arguments['--print-command']:
+        print(srun_command)
+        exit()
+
+    try:
+        run_command(srun_command)
+
+    except KeyboardInterrupt:
+        exit('Interrupt detected! exiting...')
