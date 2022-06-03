59323d11eb786eb8f4acca6a33a03e011a63bab6
Daniel <djperrefort@gmail.com>
2022-06-03 15:26:41 -0400

    Rewrites squeue wrapper to use base parser

--- crc-squeue.py
+++ crc-squeue.py
@@ -1,53 +1,61 @@
 #!/usr/bin/env /ihome/crc/wrappers/py_wrap.sh
-"""crc-sqeueue.py -- An squeue Slurm helper
-Usage:
-    crc-squeue.py [-hvasw]
+"""A simple wrapper around the Slurm squeue command"""
 
-Options:
-    -h --help                       Print this screen and exit
-    -v --version                    Print the version of crc-squeue.py
-    -a --all                        Show all jobs
-    -s --start                      Add the approximate start time
-    -w --watch                      Updates information every 10 seconds
-"""
-
-from os import path
 from os import system, environ
 
-from docopt import docopt
+from _base_parser import BaseParser
 
-from _version import __version__
 
-__app_name__ = path.basename(__file__)
+class CrcSqueue(BaseParser):
+    """Command line application for fetching data from the Slurm ``squeue`` utility"""
 
-try:
-    # Useful variables to reduce duplication
-    user = "-u {0}".format(environ['USER'])
-    squeue = "squeue -M all"
-    watch = "-i 10"
+    # Formats for output data depending on user provided arguments
     output_user_format = "-o '%.7i %.3P %.35j %.2t %.12M %.6D %.4C %.20R'"
     output_all_format = "-o '%.7i %.3P %.6a %.6u %.35j %.2t %.12M %.6D %.4C %.20R'"
     output_user_format_start = "-o '%.7i %.3P %.35j %.2t %.12M %.6D %.4C %.20R %.20S'"
     output_all_format_start = "-o '%.7i %.3P %.6a %.6u %.35j %.2t %.12M %.6D %.4C %.20R %.20S'"
 
-    arguments = docopt(__doc__, version='{} version {}'.format(__app_name__, __version__))
+    def __init__(self):
+        """Define arguments for the command line interface"""
 
-    if arguments['--all'] and arguments['--start'] and arguments['--watch']:
-        system("{0} {1} {2}".format(squeue, watch, output_all_format_start))
-    elif arguments['--all'] and arguments['--start']:
-        system("{0} {1}".format(squeue, output_all_format_start))
-    elif arguments['--all'] and arguments['--watch']:
-        system("{0} {1} {2}".format(squeue, watch, output_all_format))
-    elif arguments['--start'] and arguments['--watch']:
-        system("{0} {1} {2} {3}".format(squeue, user, watch, output_user_format_start))
-    elif arguments['--all']:
-        system("{0} {1}".format(squeue, output_all_format))
-    elif arguments['--start']:
-        system("{0} {1} {2}".format(squeue, user, output_user_format_start))
-    elif arguments['--watch']:
-        system("{0} {1} {2} {3}".format(squeue, user, watch, output_user_format))
-    else:
-        system("{0} {1} {2}".format(squeue, user, output_user_format))
+        super(CrcSqueue, self).__init__()
+        self.add_argument('-a', '--all', action='store true', help="show all jobs")
+        self.add_argument('-s', '--start', action='store true', help="add the approximate start time")
+        self.add_argument('-w', '--watch', action='store true', help="updates information every 10 seconds")
 
-except KeyboardInterrupt:
-    exit('Interrupt detected! exiting...')
+    def app_logic(self, args):
+
+        # Useful variables for building shell commands
+        user = "-u {0}".format(environ['USER'])
+        squeue = "squeue -M all"
+        watch = "-i 10"
+
+        if args.all and args.start and args.watch:
+            command = "{0} {1} {2}".format(squeue, watch, self.output_all_format_start)
+
+        elif args.all and args.start:
+            command = "{0} {1}".format(squeue, self.output_all_format_start)
+
+        elif args.all and args.watch:
+            command = "{0} {1} {2}".format(squeue, watch, self.output_all_format)
+
+        elif args.start and args.watch:
+            command = "{0} {1} {2} {3}".format(squeue, user, watch, self.output_user_format_start)
+
+        elif args.all:
+            command = "{0} {1}".format(squeue, self.output_all_format)
+
+        elif args.start:
+            command = "{0} {1} {2}".format(squeue, user, self.output_user_format_start)
+
+        elif args.watch:
+            command = "{0} {1} {2} {3}".format(squeue, user, watch, self.output_user_format)
+
+        else:
+            command = "{0} {1} {2}".format(squeue, user, self.output_user_format)
+
+        system(command)
+
+
+if __name__ == '__main__':
+    CrcSqueue().execute()
