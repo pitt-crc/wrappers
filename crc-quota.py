e2b75225d0745d186a51474467923e5836d7e5b5
Daniel <djperrefort@gmail.com>
2022-06-06 21:14:04 -0400

    Refactors crc-quota to use BaseParser class for CLI

--- crc-quota.py
+++ crc-quota.py
@@ -8,7 +8,7 @@ Usage:
     crc-quota.py --version
 
 Options:
-    -u USER --user USER         Username of quota to query
+    -u USER --user USER         
     -v --verbose                Verbose quota output                    
     -h --help                   Print this screen and exit
     --version                   Print the version of crc-quota.py
@@ -17,19 +17,13 @@ Options:
 import json
 import math
 import sys
-from os import path
 from shlex import split
 from subprocess import Popen, PIPE
 
-from docopt import docopt
-
 from _base_parser import BaseParser
 
-__version__ = BaseParser.get_semantic_version()
-__app_name__ = path.basename(__file__)
 
-
-class Quota:
+class Quota(object):
     """Class to represent a quota"""
 
     def __init__(self, name, id, size_used, size_limit):
@@ -50,10 +44,7 @@ class BeegfsQuota(Quota):
     """Class to represent a BeeGFS quota"""
 
     def __init__(self, name, id, size_used, size_limit, chunk_used, chunk_limit):
-        self.name = name
-        self.id = id
-        self.size_used = size_used
-        self.size_limit = size_limit
+        super(BeegfsQuota, self).__init__(name, id, size_used, size_limit)
         self.chunk_used = chunk_used
         self.chunk_limit = chunk_limit
 
@@ -66,10 +57,7 @@ class IhomeQuota(Quota):
     """Class to represent an iHome quota"""
 
     def __init__(self, name, id, size_used, size_limit, inodes, physical):
-        self.name = name
-        self.id = id
-        self.size_used = size_used
-        self.size_limit = size_limit
+        super(IhomeQuota, self).__init__(name, id, size_used, size_limit)
         self.inodes = inodes
         self.physical = physical
 
@@ -80,14 +68,15 @@ class IhomeQuota(Quota):
 
 def beegfs_get_quota_from_gid(group):
     """Get BeeGFS quota for a given GID"""
+
     allocation_out, allocation_err = run_command("df /bgfs/{}".format(group))
 
     if len(allocation_out) == 0:
         return None
-    else:
-        quota_out, quota_err = run_command("beegfs-ctl --getquota --gid {} --csv --storagepoolid=1".format(group))
-        result = quota_out.splitlines()[1].split(',')
-        return BeegfsQuota(result[0], result[1], result[2], result[3], result[4], result[5])
+
+    quota_out, quota_err = run_command("beegfs-ctl --getquota --gid {} --csv --storagepoolid=1".format(group))
+    result = quota_out.splitlines()[1].split(',')
+    return BeegfsQuota(result[0], result[1], result[2], result[3], result[4], result[5])
 
 
 def zfs_get_quota_from_gid(group, gid):
@@ -141,8 +130,9 @@ def ix_get_quota(group, gid):
 
 
 def convert_size(size):
-    if (size == 0):
+    if size == 0:
         return '0B'
+
     size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
     i = int(math.floor(math.log(size, 1024)))
     p = math.pow(1024, i)
@@ -155,68 +145,87 @@ def run_command(command):
     return p.communicate()
 
 
-def main():
-    arguments = docopt(__doc__, version='{} version {}'.format(__app_name__, __version__))
+class CrcQuota(BaseParser):
+    """Commandline tool for fetching a user's disk quota"""
 
-    if arguments['--user'] is None:
-        USER = run_command("id -un")[0].strip()
-        GROUP = run_command("id -gn")[0].strip()
+    def __init__(self):
+        """Define arguments for the command line interface"""
 
-        UID = run_command("id -u")[0].strip()
-        GID = run_command("id -g")[0].strip()
-    else:
-        if len(run_command("id -un {}".format(arguments['--user']))[1]) > 0:
-            sys.exit("I don't know who {} is!".format(arguments['--user']))
-        USER = run_command("id -un {}".format(arguments['--user']))[0].strip()
-        GROUP = run_command("id -gn {}".format(arguments['--user']))[0].strip()
+        super(CrcQuota, self).__init__()
+        self.add_argument('-u', '--user', default=None, help='username of quota to query')
+        self.add_argument('-v', '--verbose', action='store_true', help='verbose quota output')
 
-        UID = run_command("id -u {}".format(arguments['--user']))[0].strip()
-        GID = run_command("id -g {}".format(arguments['--user']))[0].strip()
+    def app_logic(self, args):
+        """Logic to evaluate when executing the application
 
-    bgfs_quota = beegfs_get_quota_from_gid(GROUP)
-    zfs1_quota, zfs2_quota = zfs_get_quota_from_gid(GROUP, GID)
-    ihome_quota = ihome_get_quota_from_uid(USER, UID)
-    ix_quota = ix_get_quota(GROUP, GID)
+        Args:
+            args: Parsed command line arguments
+        """
 
-    print("User: '{}'".format(USER))
-    if ihome_quota is None:
-        print("-> ihome: NONE")
-    else:
-        if arguments['--verbose'] is True:
-            print("-> ihome: {}".format(ihome_quota))
+        if args.user is None:
+            USER = run_command("id -un")[0].strip()
+            GROUP = run_command("id -gn")[0].strip()
+
+            UID = run_command("id -u")[0].strip()
+            GID = run_command("id -g")[0].strip()
         else:
-            print("-> ihome: {} / {}".format(convert_size(float(ihome_quota.size_used)), convert_size(float(ihome_quota.size_limit))))
+            if len(run_command("id -un {}".format(args.user))[1]) > 0:
+                sys.exit("I don't know who {} is!".format(args.user))
 
-    print("")
+            USER = run_command("id -un {}".format(args.user))[0].strip()
+            GROUP = run_command("id -gn {}".format(args.user))[0].strip()
 
-    print("Group: '{}'".format(GROUP))
-    if zfs1_quota is None and zfs2_quota is None and bgfs_quota is None and ix_quota is None:
-        print("If you need additional storage, you can request up to 5TB on BGFS, ZFS or IX!. Contact CRC for more details.")
+            UID = run_command("id -u {}".format(args.user))[0].strip()
+            GID = run_command("id -g {}".format(args.user))[0].strip()
 
-    if zfs1_quota is not None:
-        if arguments['--verbose'] is True:
-            print("-> zfs1: {}".format(zfs1_quota))
+        bgfs_quota = beegfs_get_quota_from_gid(GROUP)
+        zfs1_quota, zfs2_quota = zfs_get_quota_from_gid(GROUP, GID)
+        ihome_quota = ihome_get_quota_from_uid(USER, UID)
+        ix_quota = ix_get_quota(GROUP, GID)
+
+        print("User: '{}'".format(USER))
+        if ihome_quota is None:
+            print("-> ihome: NONE")
         else:
-            print("-> zfs1: {} / {}".format(convert_size(float(zfs1_quota.size_used)), convert_size(float(zfs1_quota.size_limit))))
+            if args.verbose:
+                print("-> ihome: {}".format(ihome_quota))
+            else:
+                print("-> ihome: {} / {}".format(convert_size(float(ihome_quota.size_used)), convert_size(float(ihome_quota.size_limit))))
 
-    if zfs2_quota is not None:
-        if arguments['--verbose'] is True:
-            print("-> zfs2: {}".format(zfs2_quota))
-        else:
-            print("-> zfs2: {} / {}".format(convert_size(float(zfs2_quota.size_used)), convert_size(float(zfs2_quota.size_limit))))
+        print("")
 
-    if bgfs_quota is not None:
-        if arguments['--verbose'] is True:
-            print("-> bgfs: {}".format(bgfs_quota))
-        else:
-            print("-> bgfs: {} / {}".format(convert_size(float(bgfs_quota.size_used)), convert_size(float(bgfs_quota.size_limit))))
+        print("Group: '{}'".format(GROUP))
+        if zfs1_quota is None and zfs2_quota is None and bgfs_quota is None and ix_quota is None:
+            print("If you need additional storage, you can request up to 5TB on BGFS, ZFS or IX!. Contact CRC for more details.")
 
-    if ix_quota is not None:
-        if arguments['--verbose'] is True:
-            print("-> ix: {}".format(ix_quota))
-        else:
-            print("-> ix: {} / {}".format(convert_size(float(ix_quota.size_used)), convert_size(float(ix_quota.size_limit))))
+        if zfs1_quota is not None:
+            if args.verbose:
+                print("-> zfs1: {}".format(zfs1_quota))
+
+            else:
+                print("-> zfs1: {} / {}".format(convert_size(float(zfs1_quota.size_used)), convert_size(float(zfs1_quota.size_limit))))
+
+        if zfs2_quota is not None:
+            if args.verbose:
+                print("-> zfs2: {}".format(zfs2_quota))
+
+            else:
+                print("-> zfs2: {} / {}".format(convert_size(float(zfs2_quota.size_used)), convert_size(float(zfs2_quota.size_limit))))
+
+        if bgfs_quota is not None:
+            if args.verbose:
+                print("-> bgfs: {}".format(bgfs_quota))
+
+            else:
+                print("-> bgfs: {} / {}".format(convert_size(float(bgfs_quota.size_used)), convert_size(float(bgfs_quota.size_limit))))
+
+        if ix_quota is not None:
+            if args.verbose:
+                print("-> ix: {}".format(ix_quota))
+
+            else:
+                print("-> ix: {} / {}".format(convert_size(float(ix_quota.size_used)), convert_size(float(ix_quota.size_limit))))
 
 
 if __name__ == "__main__":
-    main()
+    CrcQuota().execute()
