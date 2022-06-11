77bf859f85f089aad65b50ae017a5735cf763391
Daniel <djperrefort@gmail.com>
2022-06-11 16:50:54 -0400

    Fixes typo in parsing test

--- tests/test_crc_sus.py
+++ tests/test_crc_sus.py
@@ -10,5 +10,5 @@ class ArgumentParsing(TestCase):
         """Test the account name is recovered from the command line"""
 
         account_name = 'sam'
-        args = CrcSus().parse_args(account_name)
+        args = CrcSus().parse_args([account_name])
         self.assertEqual(account_name, args.account)
