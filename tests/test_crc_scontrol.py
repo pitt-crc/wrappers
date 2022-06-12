fc30bc4b04384a55dddecfeebdf9b80105ec344a
Daniel <djperrefort@gmail.com>
2022-06-12 14:14:33 -0400

    Adds test for help text on invlaid arg combination

--- tests/test_crc_scontrol.py
+++ tests/test_crc_scontrol.py
@@ -1,6 +1,7 @@
 """Tests for the ``crc-scontrol`` application."""
 
 from unittest import TestCase
+from unittest.mock import patch
 
 from _base_parser import CommonSettings
 
@@ -19,3 +20,18 @@ class ArgumentParsing(TestCase):
             known_args, unknown_args = app.parse_known_args(['--cluster', cluster])
             self.assertEqual(cluster, known_args.cluster)
             self.assertFalse(unknown_args)
+
+
+class AppLogic(TestCase):
+    """test the routing of app logic based on command-line arguments"""
+
+    @patch('crc-scontrol.CrcScontrol.print_help')
+    def test_help_text_for_cluster_and_partition(self, mock_print_help):
+        """Ensure help text is printed when ``--cluster`` and ``--partition`` are specified"""
+
+        app = CrcScontrol()
+        args, unknown_args = app.parse_known_args(['--cluster', 'smp', '--partition', 'smp'])
+        self.assertFalse(unknown_args)
+
+        app.app_logic(args)
+        mock_print_help.assert_called()
