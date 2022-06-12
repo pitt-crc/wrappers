b22ad382f55aada10930afd0c0bf14c7c3b70320
Daniel <djperrefort@gmail.com>
2022-06-12 10:42:50 -0400

    Seperates logic for fetching partition info into dedicated method

--- crc-scontrol.py
+++ crc-scontrol.py
@@ -16,6 +16,24 @@ class CrcScontrol(BaseParser, CommonSettings):
         self.add_argument('-c', '--cluster', choices=valid_clusters, help='print partitions for the given cluster')
         self.add_argument('-p', '--partition', help='print information about nodes in the given partition')
 
+    def get_partition_info(self, cluster, partition):
+        """Return a dictionary of Slurm settings as configured on a given partition
+
+        Args:
+            cluster: The name of the cluster to get settings for
+            partition: The name of the partition in the given cluster
+        """
+
+        scontrol_command = "scontrol -M {} show partition {}".format(cluster, partition)
+        cmd_out = self.run_command(scontrol_command).split()
+
+        partition_info = {}
+        for slurm_option in cmd_out:
+            option_name, option_value = slurm_option.split('=')
+            partition_info[option_name] = option_value
+
+        return partition_info
+
     def print_node(self, cluster, partition):
         """Print the slurm configuration of a given cluster and partition
 
@@ -24,16 +42,15 @@ class CrcScontrol(BaseParser, CommonSettings):
             partition: The name of the partition on the cluster
         """
 
-        command = "scontrol -M {} show partition {}".format(cluster, partition)
+        # Retrieve a list of nodes available in a given partition
+        partition_info = self.get_partition_info(cluster, partition)
+        partition_nodes = partition_info['Nodes']
 
-        cluster_dict = {}
-        for op in self.run_command(command).split():
-            sp = op.split('=')
-            cluster_dict[sp[0]] = sp[1]
-
-        command_out = self.run_command("scontrol show hostname {}".format(cluster_dict['Nodes']))
-
-        node = command_out.split()[0]
+        # Get slurm settings for each node in the partition
+        # Only print out values for a single node
+        # Assume the first node is representative of the partition
+        nodes_info = self.run_command("scontrol show hostname {}".format(partition_nodes))
+        node = nodes_info.split()[0]
         print(self.run_command("scontrol -M {} show node {}".format(cluster, node)))
 
     def app_logic(self, args):
@@ -45,18 +62,16 @@ class CrcScontrol(BaseParser, CommonSettings):
 
         if args.cluster and args.partition:
             self.print_help()
-            self.exit(0)
 
         # If the cluster is specified, summarize the available partitions
-        if args.cluster:
+        elif args.cluster:
             print(self.run_command("scontrol -M {} show partition".format(args.cluster)))
-            self.exit(0)
 
         # If the partition is specified, find the corresponding cluster
-        for cluster, partitions in self.cluster_partitions.items():
-            if args.partition in partitions:
-                self.print_node(cluster, args.partition)
-                self.exit(0)
+        else:
+            for cluster, partitions in self.cluster_partitions.items():
+                if args.partition in partitions:
+                    self.print_node(cluster, args.partition)
 
         self.error("Error: I don't recognize partition: {}".format(args.partition))
 
