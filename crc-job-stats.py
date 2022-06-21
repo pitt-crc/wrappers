839af006fea2343ff9b35c0efadb5879c1573683
Daniel Perrefort <djperrefort@gmail.com>
2022-06-21 14:04:27 -0400

    Adds docstring and comments to crc-job-stats (#50)

--- crc-job-stats.py
+++ crc-job-stats.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env /ihome/crc/wrappers/py_wrap.sh
+"""Command line utility for printing basic information about a running job."""
+
 from os import environ
 
 from _base_parser import BaseParser
@@ -21,10 +23,12 @@ class CrcJobStats(BaseParser):
         """Return information about the running job as a dictionary"""
 
         # Get job information from the ``scontrol`` utility
+        # Slurm settings are returned as "key=value" pairs seperated by whitespace
         output = self.run_command(['scontrol', '-M', self.cluster, 'show', 'job', self.job_id])
         split_output = output.strip().split()
 
-        # Need to deal with spaces in directory names
+        # Some slurm settings are file paths which may contain whitespace
+        # Fix any broken file paths in the split settings values
         drop_indices = []
         for idx, item in enumerate(split_output):
             if '=' not in item:
@@ -34,7 +38,7 @@ class CrcJobStats(BaseParser):
         for drop_idx in reversed(sorted(drop_indices)):
             del split_output[drop_idx]
 
-        # Extract the job information
+        # Extract the job information as a dictionary
         job_info = {}
         for item in split_output:
             spl = item.split('=', 1)
@@ -42,36 +46,41 @@ class CrcJobStats(BaseParser):
 
         return job_info
 
-    def pretty_print_job_info(self, job_info):
+    @staticmethod
+    def pretty_print_job_info(job_info):
         """Print information about a running job in a readable format
 
         Args:
             job_info: A dictionary of information about the running job
         """
 
-        # Print the output header
         width = 78
-        print('=' * width)
+        horizontal_border = '=' * width
+        custom_slurm_command = '`sacct -M {} -j {} -S {} -E {}`'.format(
+            environ['SLURM_CLUSTER_NAME'], job_info['JobId'], job_info['SubmitTime'], job_info['EndTime'])
+
+        # Print the output header
+        print(horizontal_border)
         print('JOB STATISTICS'.center(width))
-        print('=' * width)
+        print(horizontal_border)
         print('')
 
         # Print metrics for running jobs
-        for item in ('SubmitTime', 'EndTime', 'RunTime', 'JobId', 'TRES', 'Partition', 'NodeList', 'Command'):
+        for item in ('JobId', 'SubmitTime', 'EndTime', 'RunTime', 'TRES', 'Partition', 'NodeList', 'Command'):
             print('{:>16s}: {}'.format(item, job_info[item]))
 
         # Add the more information section
         print('')
-        print('=' * width)
+        print(horizontal_border)
         print(' For more information use the command:')
-        print('   - `sacct -M {} -j {} -S {} -E {}`'.format(environ['SLURM_CLUSTER_NAME'], job_info['JobId'], job_info['SubmitTime'], job_info['EndTime']))
+        print('   - '.format(custom_slurm_command))
         print('')
-        print(' To control the output of the bove command:')
+        print(' To control the output of the above command:')
         print('   - Add `--format=<field1,field2,etc>` with fields of interest')
         print('   - See the list of all possible fields by running: `sacct --helpformat`')
 
         # End the table
-        print('=' * width)
+        print(horizontal_border)
 
     def app_logic(self, args):
         """Logic to evaluate when executing the application
