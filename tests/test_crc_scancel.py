d9ae72cd15bf99c9714d7fa0d13001d5a5555e2e
Daniel <djperrefort@gmail.com>
2022-06-21 09:44:28 -0400

    Updates unittests

--- tests/test_crc_scancel.py
+++ tests/test_crc_scancel.py
@@ -9,10 +9,14 @@ class ArgumentParsing(TestCase):
     """Test the parsing of command line arguments"""
 
     def test_job_id_is_parsed(self):
-        """Test the job id is recovered from the command line"""
+        """Test the job id is recovered from the command line
 
-        job_id = 1234
-        args, unknown_args = CrcSCancel().parse_known_args([str(job_id)])
+        This test ensures the correct value is parsed AND the
+        value is parsed as a string.
+        """
+
+        job_id = '1234'
+        args, unknown_args = CrcSCancel().parse_known_args([job_id])
 
         self.assertFalse(unknown_args)
         self.assertEqual(job_id, args.job_id)
