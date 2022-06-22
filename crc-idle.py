026398a307a535460d218dd1e8c93d26d95911f1
Daniel <djperrefort@gmail.com>
2022-06-22 13:35:22 -0400

    Refactors CrcIdle class so methods better ahere to SRP

--- crc-idle.py
+++ crc-idle.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env /ihome/crc/wrappers/py_wrap.sh
 """Command line application for listing idle Slurm resources"""
 
-from shlex import split
-from subprocess import Popen, PIPE
-
 from _base_parser import BaseParser, CommonSettings
 
 
@@ -13,10 +10,10 @@ class CrcIdle(BaseParser, CommonSettings):
     # The type of resource available on a cluster
     # Either ``cores`` or ``GPUs`` depending on the cluster type
     cluster_types = {
-        "smp": 'cores',
-        "gpu": 'GPUs',
-        "mpi": 'cores',
-        "htc": 'cores'
+        'smp': 'cores',
+        'gpu': 'GPUs',
+        'mpi': 'cores',
+        'htc': 'cores'
     }
 
     def __init__(self):
@@ -28,7 +25,59 @@ class CrcIdle(BaseParser, CommonSettings):
         self.add_argument('-m', '--mpi', action='store_true', help='show idle resources on the mpi cluster')
         self.add_argument('-i', '--invest', action='store_true', help='show idle resources on the invest cluster')
         self.add_argument('-d', '--htc', action='store_true', help='show idle resources on the htc cluster')
-        self.add_argument('-p', '--partition', help='Specify non-default partition')
+        self.add_argument('-p', '--partition', nargs='+', help='Specify non-default partition')
+
+    def get_cluster_list(self, args):
+        """Return a list of clusters specified in the command line arguments
+
+        Returns a tuple of clusters specified by command line arguments. If no
+        clusters were specified, then return a tuple of all cluster names.
+
+        Args:
+            args: Parsed command line arguments
+
+        Returns:
+            A tuple fo cluster names
+        """
+
+        all_clusters = tuple(self.cluster_partitions)
+        argument_clusters = filter(lambda cluster: getattr(args, cluster), self.cluster_partitions)
+
+        # Default to returning all clusters
+        return argument_clusters or all_clusters
+
+    def count_idle_resources(self, cluster, partition):
+        """Determine the number of idle resources on a given cluster partition
+
+        The returned dictionary maps the number of idle resources (e.g., cores)
+        to the number of nodes in the partition having that many resources idle.
+
+        Args:
+            cluster: The cluster to print a summary for
+            partition: The partition in the parent cluster
+
+        Returns:
+            A dictionary mapping idle resources to number of nodes
+        """
+
+        # Use `sinfo` command to determine the status of each node in the given partition
+        command = 'sinfo -M {0} -p {1} -N -o %N,%C'.format(cluster, partition)
+        stdout = self.run_command(command)
+
+        first_line_of_data = 3
+        slurm_data = stdout.strip().split()[first_line_of_data:]
+
+        # Count the number of nodes having a given number of idle cores
+        return_dict = {}
+        for node_info in slurm_data:
+            node_name, resource_data = node_info.split(',')
+            allocated, idle, other, total = [int(x) for x in resource_data.split('/')]
+
+            if idle > 0:
+                return_dict.setdefault(idle, 0)
+                return_dict[idle] += 1
+
+        return return_dict
 
     def print_partition_summary(self, cluster, partition):
         """Print a summary of idle resources in a single partition
@@ -38,33 +87,19 @@ class CrcIdle(BaseParser, CommonSettings):
             partition: The partition in the parent cluster
         """
 
-        # Use `sinfo` command to determine the status of each node in the given partition
-        first_line_of_data = 3
-        command = "sinfo -M {0} -p {1} -N -o %N,%C".format(cluster, partition)
-        stdout, stderr = Popen(split(command), stdout=PIPE, stderr=PIPE).communicate()
-        out = stdout.strip().split()[first_line_of_data:]
+        resource_allocation = self.count_idle_resources(cluster, partition)
 
-        # Count the number of nodes having a given number of idle cores
-        core_dict = {}
-        for line in out:
-            node_name, resource_data = line.split(",")
-            _, idle, _, total = [int(x) for x in resource_data.split("/")]
-
-            if idle != 0:
-                core_dict.setdefault(idle, 0)
-                core_dict[idle] += 1
-
-        # Print results for the current partition
-        clus_par_str = "Cluster: {0}, Partition: {1}".format(cluster, partition)
-        print(clus_par_str)
-        print("=" * len(clus_par_str))
-
+        output_width = 30
+        header = 'Cluster: {0}, Partition: {1}'.format(cluster, partition)
         unit = self.cluster_types[cluster]
-        for idle in sorted(core_dict.keys()):
-            print("{0:3d} nodes w/ {1:3d} idle {2}".format(core_dict[idle], idle, unit))
 
-        if not core_dict:
-            print(" No idle resources")
+        print(header.center(output_width))
+        print('=' * output_width)
+        for idle, nodes in sorted(resource_allocation.items()):
+            print('{0:3d} nodes w/ {1:3d} idle {2}'.format(nodes, idle, unit))
+
+        if not resource_allocation:
+            print(' No idle resources')
 
         print('')
 
@@ -75,13 +110,8 @@ class CrcIdle(BaseParser, CommonSettings):
             args: Parsed command line arguments
         """
 
-        # Check which clusters to print idle resources for. Default to all clusters.
-        clusters = tuple(clus for clus in self.cluster_partitions if getattr(args, clus))
-        if not clusters:
-            clusters = tuple(self.cluster_partitions)
-
-        for cluster in clusters:
-            partitions_to_print = [args.partition] if args.partition else self.cluster_partitions[cluster]
+        for cluster in self.get_cluster_list(args):
+            partitions_to_print = args.partition or self.cluster_partitions[cluster]
             for partition in partitions_to_print:
                 self.print_partition_summary(cluster, partition)
 
