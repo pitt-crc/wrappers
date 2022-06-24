c5e73a9ba0f669624f7a4604541428b3ba7b8e17
Daniel <djperrefort@gmail.com>
2022-06-24 09:40:32 -0400

    Adds deprecation notice for old command name

---
+++ crc-scontrol.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env /ihome/crc/wrappers/py_wrap.sh
+
+print(
+    'DEPRECATION WARNING: The `crc-scontrol` application has been deprecated. '
+    'Please use the `crc-show-config` application instead.'
+)
