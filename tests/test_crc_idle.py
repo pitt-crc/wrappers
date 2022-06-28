bf27b114a98394fef07493300cba1869e8c36003
Daniel Perrefort <djperrefort@gmail.com>
2022-06-28 14:37:58 -0400

    Bug fixes to crc-idle.py (#14)
    * Refactors crc-idle
    
    * Simplifies argument verification
    
    * Moves scripting logic under main
    
    * Fixes missing output data
    
    * Removes error if partition not specified in app settings
    
    * Removes scratch file
    
    * Refactors crc-idle to be subclass of base parser
    
    * Removes scratch file
    
    * Refactors CrcIdle class so methods better ahere to SRP
    
    * Adds tests for CrcIdle argument parsing
    
    * Adds tests for cluster selection in crc-idle
    
    * Adds some additional docstring
    
    * fix GPU counting
    
    * remove redundant conditional
    
    * small codacy items
    
    * Moves CPU and GPU logic into separate methods
    
    Co-authored-by: Nickolas Comeau <nickolas.comeau@pitt.edu>
    Co-authored-by: Nickolas Comeau <nlc60@pitt.edu>

---
+++ tests/test_crc_idle.py
@@ -0,0 +1,64 @@
+"""Tests for the ``crc-idle`` application"""
+
+from unittest import TestCase
+
+CrcIdle = __import__('crc-idle').CrcIdle
+
+
+class ArgumentParsing(TestCase):
+    """Test the parsing of command line arguments"""
+
+    def test_partition_parsing(self):
+        """Test the application supports parsing multiple cluster partitions"""
+
+        app = CrcIdle()
+        args, unknown_args = app.parse_known_args(['-p', 'partition1'])
+        self.assertFalse(unknown_args)
+        self.assertEqual(['partition1'], args.partition)
+
+        args, unknown_args = app.parse_known_args(['-p', 'partition1', 'partition2'])
+        self.assertFalse(unknown_args)
+        self.assertEqual(['partition1', 'partition2'], args.partition)
+
+    def test_cluster_parsing(self):
+        """Test argument flags are parsed and stored as cluster names"""
+
+        app = CrcIdle()
+        args, unknown_args = app.parse_known_args(['-s', '--mpi'])
+
+        self.assertFalse(unknown_args)
+        self.assertTrue(args.smp)
+        self.assertTrue(args.mpi)
+        self.assertFalse(args.htc)
+        self.assertFalse(args.gpu)
+
+    def test_clusters_default_to_false(self):
+        """Test all cluster flags default to a ``False`` value"""
+
+        app = CrcIdle()
+        args, unknown_args = app.parse_known_args([])
+
+        self.assertFalse(unknown_args)
+        for cluster in app.cluster_partitions:
+            self.assertFalse(getattr(args, cluster))
+
+
+class ClusterList(TestCase):
+    """Test the selection of what clusters to print"""
+
+    def test_defaults_all_clusters(self):
+        """Test all clusters are returned if none are specified in the parsed arguments"""
+
+        app = CrcIdle()
+        args, unknown_args = app.parse_known_args(['-p', 'partition1'])
+
+        returned_clusters = app.get_cluster_list(args)
+        self.assertCountEqual(app.cluster_partitions, returned_clusters)
+
+    def test_returns_arg_values(self):
+        """Test returned cluster names match the clusters specified in the parsed arguments"""
+        app = CrcIdle()
+        args, unknown_args = app.parse_known_args(['-s', '--mpi'])
+
+        returned_clusters = app.get_cluster_list(args)
+        self.assertCountEqual(['smp', 'mpi'], returned_clusters)
