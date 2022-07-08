c827fd88070dec6fb2b988965a5a4d31f22cc67d
Daniel <djperrefort@gmail.com>
2022-07-08 08:43:18 -0400

    Remove wrapper shell scripts

--- jupyter_labhub_wrap.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash -l
-
-exec /ihome/crc/install/python/miniconda3-3.7/bin/jupyter-labhub --config=/ihome/crc/install/python/miniconda3-3.7/etc/jupyter/jupyter_notebook_config.py $@
--- py_wrap.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-PYTHONPATH='/ihome/sam/bmooreii/workspace/wrappers:/usr/lib64/python27.zip:/usr/lib64/python2.7:/usr/lib64/python2.7/plat-linux2:/usr/lib64/python2.7/lib-tk:/usr/lib64/python2.7/lib-old:/usr/lib64/python2.7/lib-dynload:/usr/lib64/python2.7/site-packages:/usr/lib64/python2.7/site-packages/gtk-2.0:/usr/lib/python2.7/site-packages' PYTHONHOME='' exec /usr/bin/python2.7 $@
