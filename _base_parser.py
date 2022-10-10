cf971517599aaf467eed97128ef0f5f4372063e7
Daniel <djperrefort@gmail.com>
2022-10-10 08:52:32 -0400

    Fixes 2.7 compatibility

--- _base_parser.py
+++ _base_parser.py
@@ -66,7 +66,7 @@ class BaseParser(ArgumentParser):
         """Parse command line arguments and execute the application"""
 
         new_app_name = self.prog.replace('.py', '')
-        print(f'{self.prog} is being deprecated. Please use {new_app_name} instead.')
+        print('{} is being deprecated. Please use {} instead.'.format(self.prog, new_app_name))
 
         try:
             args = self.parse_args()
