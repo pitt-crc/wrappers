3b0dc7757c998dfac7d18d2b10c5deba57e49f58
chnixi <38959006+chnixi@users.noreply.github.com>
2023-03-17 17:20:14 -0400

    Changed type of default_time from int to str

--- apps/crc_interactive.py
+++ apps/crc_interactive.py
@@ -23,7 +23,7 @@ class CrcInteractive(BaseParser):
     min_time = 1  # Minimum limit on requested time in hours
     max_time = 12  # Maximum limit on requested time in hours
 
-    default_time = 1  # Default runtime
+    default_time = '1'  # Default runtime
     default_nodes = 1  # Default number of nodes
     default_cores = 1  # Default number of requested cores
     default_mem = 1  # Default memory in GB
@@ -51,7 +51,7 @@ class CrcInteractive(BaseParser):
         resource_args = self.add_argument_group('Arguments for Increased Resources')
         resource_args.add_argument('-b', '--mem', type=int, default=self.default_mem, help='memory in GB')
         resource_args.add_argument(
-            '-t', '--time',  default=str(self.default_time),
+            '-t', '--time',  default=self.default_time,
             help=f'run time in hours [default: {self.default_time}]')
 
         resource_args.add_argument(
