e0ad1994ed3c0e222f43c69be4c9f9d24ec5456c
Daniel Perrefort <djperrefort@gmail.com>
2023-04-03 11:24:22 -0400

    Adds deprecation warning to version 0.3.x (#121)
    * Add deprecation warning.
    
    * Fixes 2.7 compatibility
    
    * Format deprecation warning for better visibility
    
    * Drop .pl extensions in deprecation warning

--- _base_parser.py
+++ _base_parser.py
@@ -65,6 +65,9 @@ class BaseParser(ArgumentParser):
     def execute(self):
         """Parse command line arguments and execute the application"""
 
+        new_app_name = self.prog.replace('.py', '').replace('.pl', '')
+        print('!! {} is being deprecated. Please use {} instead.\n'.format(self.prog, new_app_name))
+
         try:
             args = self.parse_args()
             self.app_logic(args)
