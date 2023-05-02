c6c4f305b6d767f0f869f61df03db4a5a143eb53
Daniel <djperrefort@gmail.com>
2023-05-02 09:29:47 -0400

    Enable tests for hlp text

--- tests/utils/cli/test_base_parser.py
+++ tests/utils/cli/test_base_parser.py
@@ -1,6 +1,6 @@
-"""Tests for the ``BaseParser`` class."""
+"""Tests for the ``Parser`` class."""
 
-from unittest import TestCase, mock, skip
+from unittest import TestCase, mock
 
 from apps.utils.cli import Parser
 
@@ -9,16 +9,18 @@ class ErrorHandling(TestCase):
     """Test error handling via the ``error`` method"""
 
     def test_raised_as_system_exit(self) -> None:
-        """Test the ``error`` method raises a ``SystemExit`` error"""
+        """Test the ``error`` method raises a ``SystemExit`` error
+
+        Ensure error messages are passed to the raised exception.
+        """
 
         message = 'This is a test'
         with self.assertRaisesRegex(SystemExit, message):
             Parser().error(message)
 
-    @skip
     @mock.patch('apps.utils.cli.Parser.print_help')
     def test_help_is_printed(self, mock_print_help: mock.Mock) -> None:
-        """Test help text is printed automatically by default"""
+        """Test help text is printed when running without commandline arguments"""
 
         with self.assertRaises(SystemExit):
             Parser().error('this is a test')
