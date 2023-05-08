06a9f97afbf4dfc032ff4056243b7be6e13f465d
Daniel Perrefort <djperrefort@gmail.com>
2023-05-08 16:44:36 -0400

    Adds -z and -c arguments to crc-sinfo (#189)
    * Adds -z and -c arguments to crc-sinfo
    
    * Updates class docs
    
    * Adds missing arguments to execute method

--- apps/crc_sinfo.py
+++ apps/crc_sinfo.py
@@ -14,7 +14,16 @@ from .utils.system_info import Shell
 
 
 class CrcSinfo(BaseParser):
-    """Display information about all available Slurm clusters."""
+    """Display information about available Slurm clusters."""
+
+    def __init__(self) -> None:
+        """Define the application commandline interface"""
+
+        super().__init__()
+        self.add_argument('-c', '--cluster', nargs='?', default='all', help='only show jobs for the given cluster')
+        self.add_argument(
+            '-z', '--print-command', action='store_true',
+            help='print the equivalent slurm command and exit')
 
     def app_logic(self, args: Namespace) -> None:
         """Logic to evaluate when executing the application
@@ -23,4 +32,9 @@ class CrcSinfo(BaseParser):
             args: Parsed command line arguments
         """
 
-        print(Shell.run_command("sinfo -M all"))
+        command = f'sinfo -M {args.cluster}'
+        if args.print_command:
+            print(command)
+
+        else:
+            print(Shell.run_command(command))
