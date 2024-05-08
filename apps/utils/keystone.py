21b178b3d8749527c428625d5bd0cb60ba8cff24
Nickolas Comeau <nlc60@pitt.edu>
2024-05-08 15:52:45 -0400

    researchgroup -> researchgroup ID, per_request option for per cluster totals

--- apps/utils/keystone.py
+++ apps/utils/keystone.py
@@ -35,12 +35,17 @@ def get_active_requests(keystone_url: str, group_pk: int, auth_header: dict) -> 
 
 
 def get_researchgroup_id(keystone_url: str, account_name: str, auth_header: dict) -> int:
-    """Get all Resource Allocation Request information from keystone"""
+    """Get the Researchgroup ID from keystone for the specified Slurm account"""
 
     response = requests.get(f"{keystone_url}/users/researchgroups/?name={account_name}", headers=auth_header)
     response.raise_for_status()
-    return int(response.json()['id'])
 
+    try:
+        group_id = int(response.json()[0]['id'])
+    except:
+        group_id = None
+
+    return group_id
 
 def get_earliest_startdate(alloc_requests: [dict]) -> date:
     """Given a number of requests, determine the earliest start date across them"""
@@ -53,14 +58,21 @@ def get_earliest_startdate(alloc_requests: [dict]) -> date:
 
     return earliest_date
 
-
-def get_awarded_totals(alloc_requests: [dict], auth_header: dict) -> dict:
+def get_per_cluster_totals(alloc_requests: [dict], auth_header: dict, per_request: bool = False) -> dict:
     """Gather the awarded totals across the given requests on each cluster into a dictionary"""
+
     per_cluster_totals = {}
     for request in alloc_requests:
+        if per_request:
+            per_cluster_totals[request['id']] = {}
         for allocation in get_request_allocations(KEYSTONE_URL, request['id'], auth_header):
             cluster = CLUSTERS[allocation['cluster']]
-            per_cluster_totals.setdefault(cluster, 0)
-            per_cluster_totals[cluster] += allocation['awarded']
+            if per_request:
+                per_cluster_totals[request['id']].setdefault(cluster,0)
+                per_cluster_totals[request['id']][cluster] += allocation['awarded']
+            else:
+                per_cluster_totals.setdefault(cluster, 0)
+                per_cluster_totals[cluster] += allocation['awarded']
 
     return per_cluster_totals
+
