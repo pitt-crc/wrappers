80d4648b00591cf5d8a437e5ac59d7d7a2ea1b65
Comeani <nlc60@pitt.edu>
2024-05-16 16:05:02 -0400

    make the message about not finding active allocations stand out more

--- apps/crc_proposal_end.py
+++ apps/crc_proposal_end.py
@@ -40,7 +40,7 @@ class CrcProposalEnd(BaseParser):
         alloc_requests = get_active_requests(KEYSTONE_URL, keystone_group_id, auth_header)
 
         if not alloc_requests:
-            print(f"No active allocation information found in accounting system for '{args.account}'")
+            print(f"No active allocation information found in accounting system for '{args.account}!\n'")
             print("Showing end date for most recently expired Resource Allocation Request:")
             alloc_requests = get_most_recent_expired_request(KEYSTONE_URL, keystone_group_id, auth_header)
 
