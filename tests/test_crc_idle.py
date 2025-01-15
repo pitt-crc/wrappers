632bde8a9da6495a5b16ee3a53db3373a69093fd
chnixi <38959006+chnixi@users.noreply.github.com>
2025-01-15 13:08:01 -0800

    Update test_crc_idle.py
    add teach to namespace

--- tests/test_crc_idle.py
+++ tests/test_crc_idle.py
@@ -54,7 +54,7 @@ class GetClusterList(TestCase):
         """Test returned values when no clusters are specified."""
 
         app = CrcIdle()
-        args = Namespace(smp=False, gpu=False, mpi=False, htc=False, partition=None)
+        args = Namespace(smp=False, gpu=False, mpi=False, htc=False, teach=fals, partition=None)
         result = app.get_cluster_list(args)
 
         expected = tuple(app.cluster_types.keys())
