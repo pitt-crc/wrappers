51930e733dd217088410213abc7e392ed7b66f6c
Nickolas Comeau <nlc60@pitt.edu>
2025-02-03 15:31:01 -0500

    codacy items

--- tests/utils/system_info/test_slurm.py
+++ tests/utils/system_info/test_slurm.py
@@ -1,14 +1,15 @@
-"""Tests for the ``Slurm`` class"""
-
 from unittest import TestCase
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from datetime import date
-from apps.utils.system_info import Slurm, Shell
+from apps.utils.system_info import Slurm
 
 class TestSlurm(TestCase):
+    """ Tests for the `Slurm` class """
 
     @patch('apps.utils.system_info.Shell.run_command')
     def test_is_installed(self, mock_run_command):
+        """ Test the system check for a valid installation of Slurm """
+
         # sacctmgr is installed
         mock_run_command.return_value = "slurm 22.05.11"
         self.assertTrue(Slurm.is_installed())
@@ -19,6 +20,8 @@ class TestSlurm(TestCase):
 
     @patch('apps.utils.system_info.Shell.run_command')
     def test_get_cluster_names(self, mock_run_command):
+        """ Test that cluster names are handled properly """
+
         # Mock the output of squeue command
         mock_run_command.return_value = "CLUSTER: cluster1\nCLUSTER: cluster2\nCLUSTER: azure\n"
 
@@ -32,6 +35,8 @@ class TestSlurm(TestCase):
 
     @patch('apps.utils.system_info.Shell.run_command')
     def test_get_partition_names(self, mock_run_command):
+        """ Test that partition names are handled properly """
+
         # Mock the output of scontrol command
         mock_run_command.return_value = "PartitionName=partition1\nPartitionName=partition2\nPartitionName=pliu\n"
 
@@ -45,6 +50,8 @@ class TestSlurm(TestCase):
 
     @patch('apps.utils.system_info.Shell.run_command')
     def test_check_slurm_account_exists(self, mock_run_command):
+        """ Test check for slurm account existence """
+
         # Test when account exists
         mock_run_command.return_value = "account1"
         Slurm.check_slurm_account_exists('account1')  # Should not raise an exception
@@ -56,6 +63,8 @@ class TestSlurm(TestCase):
 
     @patch('apps.utils.system_info.Shell.run_command')
     def test_get_cluster_usage_by_user(self, mock_run_command):
+        """ Test gathering usage per user from sreport """
+
         # Mock the output of sreport command
         mock_run_command.return_value = "user1|100\nuser2|200\n|300"
 
@@ -78,7 +87,4 @@ class TestSlurm(TestCase):
         mock_run_command.side_effect = ValueError
         usage = Slurm.get_cluster_usage_by_user('account1', start_date, 'cluster1')
         self.assertIsNone(usage)
-
-if __name__ == '__main__':
-    unittest.main()
 
